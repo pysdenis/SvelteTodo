c3e7e04ac0d8fb3a05b169fabc187c628bee0858
Denis Pyš <128090772+pysdenis@users.noreply.github.com>
2023-03-30 20:58:10 +0200

    filters

---
+++ src/components/Filters.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { createEventDispatcher } from "svelte"; 
+  export let filterStatus = "all"; 
+
+  const dispatch = createEventDispatcher(); 
+
+  function filterTodos(status) {
+    filterStatus = status;
+    dispatch("changeFilter", filterStatus);
+  }
+</script>
+
+<div class="d-flex justify-content-end mt-2">
+  <div class="btn-group btn-group-sm">
+    <button
+      class="btn btn-outline-secondary"
+      class:selected={filterStatus === "all"}
+      on:click={() => filterTodos("all")}
+    >
+      Všechny
+    </button>
+
+    <button
+      class="btn btn-outline-secondary"
+      class:selected={filterStatus === "active"}
+      on:click={() => filterTodos("active")}
+    >
+      Nedokončené
+    </button>
+
+    <button
+      class="btn btn-outline-secondary"
+      class:selected={filterStatus === "completed"}
+      on:click={() => filterTodos("completed")}
+    >
+      Dokončené
+    </button>
+
+  </div>
+</div>
--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -1,23 +1,44 @@
 <script>
   import AddTodo from "./AddTodo.svelte";
   import Todo from "./Todo.svelte";
+  import Filters from "./Filters.svelte";
 
   let todoItems = [];
+  let filteredItems = [];
+  let filterStatus = "all"; 
 
   function addTodo({ detail: todo }) {
     todoItems = [...todoItems, todo];
+    filterTodos(filterStatus);
   }
 
   function removeTodo({ detail: index }) {
     todoItems.splice(index, 1);
     todoItems = todoItems;
+    filterTodos(filterStatus);
+  }
+
+  function filterTodos(status) {
+    if (status === "all") {
+      filteredItems = [...todoItems];
+    } else if (status === "active") {
+      filteredItems = todoItems.filter((item) => !item.status);
+    } else if (status === "completed") {
+      filteredItems = todoItems.filter((item) => item.status);
+    }
+  }
+
+  function handleFilterChange(event) {
+    filterTodos(event.detail);
   }
 </script>
 
 <div class="container mt-5 todoBox py-4">
   <AddTodo on:addTodo={addTodo} />
 
-  {#if todoItems.length != 0}
+  
+
+  {#if filteredItems.length != 0}
     <div class="row justify-content-center mt-4">
       <div class="col-10">
         <table class="table">
@@ -32,21 +53,24 @@
             </tr>
           </thead>
           <tbody>
-            {#each todoItems as item, index}
+            {#each filteredItems as item, index}
               <Todo {index} todo={item} on:removeTodo={removeTodo} />
             {/each}
           </tbody>
         </table>
       </div>
     </div>
-    {#if todoItems.length == 1}
-      <p class="text-muted mb-0 mt-2">Zbývá ti {todoItems.length} úkol</p>
-    {:else if todoItems.length > 1 && todoItems.length < 10}
-      <p class="text-muted mb-0 mt-2">Zbývájí ti {todoItems.length} úkoly</p>
-    {:else}
-      <p class="text-muted mb-0 mt-2">Zbývá ti {todoItems.length} úkolů</p>
+    <div class="d-flex justify-content-between">
+      {#if filteredItems.length == 1}
+          <p class="text-muted mb-0 mt-3">Zbývá ti {filteredItems.length} úkol</p>
+        {:else if filteredItems.length > 1 && filteredItems.length < 10}
+          <p class="text-muted mb-0 mt-3">Zbývájí ti {filteredItems.length} úkoly</p>
+        {:else}
+          <p class="text-muted mb-0 mt-3">Zbývá ti {filteredItems.length} úkolů</p>
+      {/if}
+      <Filters on:changeFilter={handleFilterChange} />
+    </div>
     {/if}
-  {/if}
 </div>
 
 <style>
